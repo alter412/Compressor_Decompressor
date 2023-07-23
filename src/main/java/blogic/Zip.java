c965926c6b8620ccb81ebb605e801c235d1fba62
alter412 <139802395+alter412@users.noreply.github.com>
2023-07-24 00:03:10 +0530

    Comment added

--- src/main/java/blogic/Zip.java
+++ src/main/java/blogic/Zip.java
@@ -1,5 +1,5 @@
 package blogic;
-
+// importing required classes
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,77 +10,101 @@ import java.io.IOException;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
+// creating actionListener for compressor and decompressor buttons
 public class Zip implements ActionListener {
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        // fetching the command to which button was pressed
         String command = e.getActionCommand();
 
+        // compressed button pressed
         if (command.equals("compress")) {
+            // open file chooser
             JFileChooser jfc = new JFileChooser();
             int val = jfc.showOpenDialog(null);
+            // if some was selected only then perform the action
             if (val == jfc.APPROVE_OPTION) {
+                // fetch the selected file
                 String file = jfc.getSelectedFile().toString();
+               // fetch the parent directory, to be used later while saving compressed file
                 String parent = jfc.getCurrentDirectory().toString();
+                // using required file streams
                 FileInputStream fis = null;
                 FileOutputStream fos = null;
+                // used for compression
                 GZIPOutputStream gzip = null;
 
                 try {
                  fis = new FileInputStream(file);
+                 // save the compressed file at the given path
                  fos = new FileOutputStream(parent + "/" + "compressedFile");
                  gzip = new GZIPOutputStream(fos);
 
                     byte[] arr = new byte[1024];
                     int l;
+                    // write using not reached the end of file
                     while ((l = fis.read(arr)) != -1) {
                         gzip.write(arr, 0, l);
                     }
+                    // close all streams
                     fis.close();
-                  //  fos.close();
                     gzip.close();
+                    // conformation message dialog
                     JOptionPane.showMessageDialog(null,"File compressed successfully!","Information",JOptionPane.INFORMATION_MESSAGE,null);
 
                 } catch (FileNotFoundException ex) {
+                    // if file not found then this error dialog will be displayed
                     JOptionPane.showMessageDialog(null,"File not found!","Error",JOptionPane.ERROR_MESSAGE,null);
                     throw new RuntimeException(ex);
                 } catch (IOException ex) {
+                    // if io error occurred
                     JOptionPane.showMessageDialog(null,"IO Error occur try again!","Error",JOptionPane.ERROR_MESSAGE,null);
                     throw new RuntimeException(ex);
                 }
             }
 
         } else if (command.equals("decompress")) {
+            // open the file chooser
             JFileChooser jfc = new JFileChooser();
             int val = jfc.showOpenDialog(null);
+            // perform action only when file is selected
             if (val == jfc.APPROVE_OPTION) {
+                // fetch the file name
                 String file = jfc.getSelectedFile().toString();
+                // fetch the current directory
                 String parent = jfc.getCurrentDirectory().toString();
+                // using required file streams
                 FileInputStream fis = null;
                 FileOutputStream fos = null;
+                // used while decompressing
                 GZIPInputStream gzip = null;
 
                 try {
                     fis = new FileInputStream(file);
+                    // save the file at the given location
                     fos = new FileOutputStream(parent + "/" + "decompressedFile");
                     gzip = new GZIPInputStream(fis);
 
                     byte[] arr = new byte[1024];
                     int l;
+                    // write while not reached end of file
                     while ((l = gzip.read(arr)) != -1) {
                         fos.write(arr, 0, l);
                     }
-                    //fis.close();
+                    // close all streams
                     fos.close();
                     gzip.close();
+                    // conformation dialog message
                     JOptionPane.showMessageDialog(null,"File decompressed successfully!","Information",JOptionPane.INFORMATION_MESSAGE,null);
 
                 } catch (FileNotFoundException ex) {
+                    // error dialog when file not found
                     JOptionPane.showMessageDialog(null,"File not found!","Error",JOptionPane.ERROR_MESSAGE,null);
                     throw new RuntimeException(ex);
                 } catch (IOException ex) {
+                    // error dialog when io error occurred
                     JOptionPane.showMessageDialog(null,"IO Error occur try again!","Error",JOptionPane.ERROR_MESSAGE,null);
                     throw new RuntimeException(ex);
                 }
--- src/main/java/ui/Front.java
+++ src/main/java/ui/Front.java
@@ -5,34 +5,50 @@ import java.awt.*;
 import blogic.Zip;
 
 class Front{
+    // main frame window
     JFrame win;
+    // compress and decompress button
     JButton compress,decompress;
     Front(){
+        // set the title
         win = new JFrame("Compressor Decompressor");
+        // set the default close operation behavior
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // make the frame visible
         win.setVisible(true);
+        // set background as black
         win.getContentPane().setBackground(Color.black);
+        // set layout to null for full control
         win.setLayout(null);
+        // set the bounds of frame
         win.setBounds(100,100,800,800);
 
+        // set the button text
         compress = new JButton("compress");
         compress.setFocusable(false);
+        // set the size and bounds
         compress.setSize(50,50);
         compress.setBounds(400,150,150,100);
+        // add action listener
         compress.addActionListener(new Zip());
 
+        // set the button text
         decompress = new JButton("decompress");
         decompress.setFocusable(false);
+        // set the size and bounds
         decompress.setSize(150,150);
         decompress.setBounds(700,150,150,100);
+        // set the action listener
         decompress.addActionListener(new Zip());
 
+        // add both buttons to the main frame window
         win.add(compress);
         win.add(decompress);
 
 
     }
 
+    // main method where the Front class object is created
     public static void main(String[] args) {
         Front f = new Front();
     }
